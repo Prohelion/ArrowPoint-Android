29f14c97b8c289b3d64c5978eb255fc8c25c6933
Cameron Tuesley <ctuesley@integraltech.com.au>
2018-08-05 20:41:27 +1000

    Changes to reflect further build issues with new libraries and versions

--- app/src/main/java/au/com/teamarrow/arrowpoint/utils/AutoResizeTextView.java
+++ app/src/main/java/au/com/teamarrow/arrowpoint/utils/AutoResizeTextView.java
@@ -17,6 +17,7 @@ package au.com.teamarrow.arrowpoint.utils;
  */
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
 import android.text.Layout.Alignment;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -32,7 +33,7 @@ import android.widget.TextView;
  * @author Chase Colburn
  * @since Apr 4, 2011
  */
-public class AutoResizeTextView extends TextView {
+public class AutoResizeTextView extends AppCompatTextView {
 
     // Minimum text size for this text view
     public static final float MIN_TEXT_SIZE = 20;
